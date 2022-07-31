8c906c653cd66f6ca50661e65e3f71fe1d32d0f8
Łukasz Pusz <pusz.lukasz@gmail.com>
2022-07-31 14:29:27 +0200

    Small clean up

--- ninja_trips/lib/shared/heart.dart
+++ ninja_trips/lib/shared/heart.dart
@@ -6,14 +6,6 @@ class Heart extends StatefulWidget {
 }
 
 class _HeartState extends State<Heart> {
-  late String? test;
-
-  @override
-  void initState() {
-    super.initState();
-    test = '';
-  }
-
   @override
   Widget build(BuildContext context) {
     return IconButton(
